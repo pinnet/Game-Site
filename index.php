c65bb27e217fc4d351a8cafd9385c652deef30f0
Danny Arnold <danny@dannyarnold.com>
2017-10-09 18:36:16 +0100

    Connection sucessfull mqtt

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.js" type="text/javascript"></script>
     <script src="Build/UnityLoader.js"></script>
+    <script src="js/IndexDB.js<?php echo filemtime('js/IndexDB.js'); ?>" type="text/javascript"></script>
     <script src="js/main.js?<?php echo filemtime('js/main.js'); ?>"></script>
     <link rel="stylesheet" href="css/main.css?<?php echo filemtime('css/main.css'); ?>">
   </head>
--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-    
     $(function(){
         var ticks = 0;
         $('#cover').show();
         clock = setInterval(function(){
-          if (ticks < 60){
+          if (ticks < 120){
               $("#notice").append(".");
               ticks++;
           }
@@ -15,15 +14,47 @@
         window.addEventListener('UNITYReady', UnityReady);
     }
     );
+    var port=443;
+    var host="iot.eclipse.org";
+   
+   var options = {
+        
+       
+        useSSL: true,
+        onSuccess: onConnect,
+        onFailure: onConnectionLost
+    };
+   
     var gameInstance = UnityLoader.instantiate("gameContainer", "Build/unityout.json");
-    
-    
+        
     function UnityReady(){
         
        clearInterval(clock);
         
        $('#cover').hide();
         
-       console.log(localStorage.getItem("ID"));
-        
-    }  
+       client = new Paho.MQTT.Client(host,port, localStorage.getItem("ID"));
+       client.onConnectionLost = onConnectionLost;
+       client.onMessageArrived = onMessageArrived;
+       client.connect(options);        
+    };     
+ function onConnect() {
+  // Once a connection has been made, make a subscription and send a message.
+  
+  var playerId = localStorage.getItem("ID");
+  var playerName = localStorage.getItem("Name");
+  console.log("onConnect");
+  client.subscribe("/CHNT:REG:");
+  client.subscribe(playerId);
+  message = new Paho.MQTT.Message("REG:" + playerId + ":Name:" + playerName );
+  message.destinationName = "/CHNT:REG:";
+  client.send(message);
+};
+    function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0)
+    console.log("onConnectionLost:"+responseObject.errorMessage);
+};
+function onMessageArrived(message) {
+  console.log("onMessageArrived:"+message.payloadString);
+  
+};
